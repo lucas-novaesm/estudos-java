15d74d8dbd4ef723e051d3fe0efa447677010b3b
Lucas Novaes <luc.novaesm@gmail.com>
2024-02-10 12:41:29 -0300

    Implementada a classe BusinessAccount

---
+++ herança/src/entities/BusinessAccount.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class BusinessAccount extends Account {
+    
+    private Double loanLimit;
+
+
+    //CONSTRUCTORS
+    public BusinessAccount(){
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+
+    //GETTERS AND SETTERS
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if(amount <= loanLimit){
+            deposit(amount);
+        }
+    }
+    
+    
+    
+}
