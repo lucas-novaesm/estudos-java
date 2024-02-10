ada616bde47d65e5855f87ebcd16235dc040b33a
Lucas Novaes <luc.novaesm@gmail.com>
2024-02-09 21:22:13 -0300

    Classes criadas e devidamente organizadas

---
+++ exercicioComposicao/src/entities/Department.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Department {
+    private String name;
+
+    //CONSTRUCTORS
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+    // GETTERS AND SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
